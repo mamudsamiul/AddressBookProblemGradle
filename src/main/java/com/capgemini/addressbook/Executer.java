504bfdd7d656d80635a98d8001737e6029434cb8
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-03 01:17:47 +0530

    (Samiul Mamud) Added: Ability to add new contact in address book

--- src/main/java/com/capgemini/addressbook/Executer.java
+++ src/main/java/com/capgemini/addressbook/Executer.java
@@ -9,7 +9,9 @@ public class Executer {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
 		ContactDetails contactDetails = new ContactDetails();
+		AddressBook addressBook = new AddressBook("first");
 		TakeInput takeInput = new TakeInput();
 		takeInput.takeInput(contactDetails, scan);
+		addressBook.AddContact(contactDetails);
 	}
 }
