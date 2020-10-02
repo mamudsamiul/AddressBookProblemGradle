7793306d119bb2fc662fdb8af5300c1a8ef85d12
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-03 00:58:18 +0530

    (Samiul Mamud) Added: Ability to create contact details logic is added

--- src/main/java/com/capgemini/addressbook/Executer.java
+++ src/main/java/com/capgemini/addressbook/Executer.java
@@ -1,7 +1,15 @@
 package com.capgemini.addressbook;
 
+import java.util.Scanner;
+
+import com.capgemini.addressbook.dto.*;
+
 public class Executer {
 	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
 		System.out.println("Welcome to Address Book Program");
+		ContactDetails contactDetails = new ContactDetails();
+		TakeInput takeInput = new TakeInput();
+		takeInput.takeInput(contactDetails, scan);
 	}
 }
