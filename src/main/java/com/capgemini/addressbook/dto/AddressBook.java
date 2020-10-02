17b39041dd574f4a34068c62e4f570e073e8b90b
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-03 03:57:17 +0530

    (Samiul Mamud) Added: Update contact details logic is added

--- src/main/java/com/capgemini/addressbook/dto/AddressBook.java
+++ src/main/java/com/capgemini/addressbook/dto/AddressBook.java
@@ -15,4 +15,23 @@ public class AddressBook {
 	public void AddContact(ContactDetails person) {
 		addressBook.add(person);
 	}
+
+	public void ShowContact() {
+		for (int i = 0; i < addressBook.size(); i++) {
+			System.out.print(i+1 + " Entry = ");
+			System.out.print(addressBook.get(i).getFirstName());
+			System.out.print(" " + addressBook.get(i).getLastName());
+			System.out.print(" " + addressBook.get(i).getAddress());
+			System.out.print(" " + addressBook.get(i).getCity());
+			System.out.print(" " + addressBook.get(i).getState());
+			System.out.print(" " + addressBook.get(i).getZip());
+			System.out.print(" " + addressBook.get(i).getPhoneNo());
+			System.out.print(" " + addressBook.get(i).getEmailAddress());
+			System.out.println();
+		}
+	}
+
+	public ArrayList<ContactDetails> getAddressBook() {
+		return addressBook;
+	}
 }
---
+++ src/main/java/com/capgemini/addressbook/service/PersonService.java
@@ -0,0 +1,10 @@
+package com.capgemini.addressbook.service;
+
+import java.util.Scanner;
+
+import com.capgemini.addressbook.dto.AddressBook;
+
+public interface PersonService {
+	public void UpdateUser(AddressBook addressBook,Scanner scan);
+
+}
